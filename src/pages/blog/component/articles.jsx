bd4bd4212dc2d468bb1833152d3647dad61d6cd3
Ramnan Ramyil <ramyilramnan@gmail.com>
2023-05-20 23:37:48 +0100

    add blogs page and article component

---
+++ src/pages/blog/component/articles.jsx
@@ -0,0 +1,12 @@
+const Article = ({ heading, description, image }) => {
+  return (
+      <div className={`w-[330px] h-[210px] flex justify-center items-start flex-col gap-5 mb-32 `}>
+
+        <img src={image} alt="" />
+        <h2 className={`font-[700] text-[20px]`}>{heading}</h2>
+        <p className={` font-[400] text-[14px] `}>{description}</p>
+      </div>
+  );
+}
+
+export default Article;
