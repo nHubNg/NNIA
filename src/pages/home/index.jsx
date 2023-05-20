617c3e2e69891043431a9b87eec58469f3c9a0ec
Desmond-Fon <desmondfon@yahoo.com>
2023-05-20 12:21:08 +0100

    completed the slider

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,30 @@
 import Header from "../../components/Header";
 import people from '../../assets/Frame 10.png'
+import person from '../../assets/Frame 20.png'
+import incubator from '../../assets/brain.png'
+import accelerator from '../../assets/accelerator.png'
+import makerspace from '../../assets/makerspace.png'
+import hackerspace from '../../assets/hackerspace.png'
+import startup from '../../assets/startup.png'
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Virtual } from "swiper";
+import "swiper/css/virtual";
+
+
+import "swiper/css/scrollbar";
+import "swiper/css/autoplay";
+
+// import required modules
+import { Scrollbar, A11y, Autoplay } from "swiper";
+import { Navigation } from "swiper";
+import { Pagination } from "swiper";
 
 const Home = () => {
     return (<div className="font-montserrat bg-background">
@@ -31,9 +56,76 @@ const Home = () => {
             </div>
         </div>
 
-        <div className={`bg-hero bg-no-repeat bg-center bg-cover h-[513px] font-montserrat font-[700] text-center flex justify-center items-center pt-30 px-[270px] text-white flex-col`}>
+        <div className="flex justify-center items-center">
+            <div className="w-[70%] flex justify-center items-center h-[500px]">
+                <Swiper
+                    spaceBetween={50}
+                    slidesPerView={3}
+                    // virtual
+                    modules={[Navigation, Pagination, Scrollbar, A11y, Virtual, Autoplay]}
+                    // navigation
+                    autoplay={{
+                        delay: 2000,
+                        disableOnInteraction: false,
+                    }}
+                    // pagination={{ clickable: true }}
+                    // scrollbar={{ draggable: true }}
+                >
+                    <SwiperSlide>
+                        <div className="flex justify-center items-center flex-col bg-white h-[280px] rounded gap-10">
+                            <img src={incubator} alt="" />
+                            <p className="font-[700] text-[22px] text-twitterBlue">Incubators</p>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="flex justify-center items-center flex-col bg-white h-[280px] rounded gap-10">
+                            <img src={accelerator} alt="" />
+                            <p className="font-[700] text-[22px] text-twitterBlue">Accelerators</p>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="flex justify-center items-center flex-col bg-white h-[280px] rounded gap-10">
+                            <img src={startup} alt="" />
+                            <p className="font-[700] text-[22px] text-twitterBlue">Startups</p>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="flex justify-center items-center flex-col bg-white h-[280px] rounded gap-10">
+                            <img src={makerspace} alt="" />
+                            <p className="font-[700] text-[22px] text-twitterBlue">Marker Space</p>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="flex justify-center items-center flex-col bg-white h-[280px] rounded gap-10">
+                            <img src={hackerspace} alt="" />
+                            <p className="font-[700] text-[22px] text-twitterBlue">Hacker Space</p>
+                        </div>
+                    </SwiperSlide>
+                </Swiper>
+            </div>
+        </div>
+
+        <div className={`bg-hero bg-no-repeat bg-center bg-cover h-[513px] font-montserrat font-[700] text-center flex justify-center items-center pt-30 mb-16 px-[270px] text-white flex-col`}>
             <h1 className='text-[30px]'>We champion policies, drive collaboration, innovation, employability and entrepreneurship through hubs.</h1>
             <button className="px-5 py-2 border-white border-2 text-white mt-16 ml-16 font-[700]">Become a Partner</button>
+        </div>
+
+
+        <div className="h-[750px] bg-twitterBlue p-10 flex">
+            <div className="w-1/2">
+                <p className="text-[30px] font-[700] text-center flex justify-center items-center">Meet our community</p>
+                <div>
+                    <div className="text-center flex justify-center items-center pt-30 px-32 text-white text-[50px]"><p>We are a <span className="font-[700]">459 Member</span> Hubs in 100 Cities & still growing!</p></div>
+                    <div>
+                        <button className="px-5 py-2 border-white border-2 text-white mt-16 ml-16 font-[700]">Become a Partner</button>
+                    </div>
+                </div>
+             </div>
+
+                <div className="w-1/2">
+                   <img src={person} alt="" />
+                </div>
+
         </div>
     </div>);
 }
