c8f32a67f23963b1e0005277fd3fc756bb11084d
Ramnan Ramyil <ramyilramnan@gmail.com>
2023-05-29 16:52:38 +0100

    added component folder in blog

---
+++ src/pages/blogDetails/component/details.jsx
@@ -0,0 +1,15 @@
+import twitter from "../../../assets/twitter-black.svg";
+import facebook from "../../../assets/facebook-black-inverted.svg";
+import linkedin from "../../../assets/linkedin-black.svg";
+import instagram from "../../../assets/instagram-black.svg";
+
+const Details = ({}) => {
+    return (
+        <div>
+            
+        </div>
+
+    );
+}
+
+export default Details;
