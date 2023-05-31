c721433b89a92f9dc1d6e3e618483cf3fe70f6f8
Ramnan Ramyil <ramyilramnan@gmail.com>
2023-05-31 12:38:52 +0100

    linked the articles to blogDetails

--- src/pages/blog/component/articles.jsx
+++ src/pages/blog/component/articles.jsx
@@ -1,11 +1,16 @@
+import { Link } from "react-router-dom";
+
 const Article = ({ heading, description, image }) => {
   return (
-      <div className={"w-[330px] flex justify-center items-start flex-col gap-5"}>
-
+    <Link to="/blogDetails">
+      <div
+        className={"w-[330px] flex justify-center items-start flex-col gap-5"}
+      >
         <img src={image} alt="" />
         <h2 className={"font-[700] text-[20px]"}>{heading}</h2>
         <p className={" font-[400] text-[14px] "}>{description}</p>
       </div>
+    </Link>
   );
 }
 
