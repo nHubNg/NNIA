e6d89521b4fdd5f079cd018c84fed6b4a6691f84
Desmond-Fon <desmondfon@yahoo.com>
2023-09-22 09:02:03 +0100

    removed typo

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -45,7 +45,10 @@ const About = () => {
             the growth of member hubs and startups.
           </p>
           <p className="md:mx-[140px]">
-          Through collaboration and mutual support, we believe that we can create a thriving startup ecosystem that benefits everyone involved, from entrepreneurs and investors to service providers and government agencies. By fostering a culture of collaboration and open communication, we hope to create a vibrant and dynamic community that attracts talent and investment, and helps to position Jos as a hub for innovation and entrepreneurship
+            Through collaboration and mutual support, we believe that we can create a thriving startup ecosystem that benefits everyone involved, from entrepreneurs and investors to service providers and government agencies.  By fostering a culture of collaboration and open communication, we
+            hope to create a vibrant and dynamic community that attracts
+            talent and investment, and helps to position Northern NIgeria as a hub for
+            innovation and entrepreneurship.
           </p>
         </div>
 
